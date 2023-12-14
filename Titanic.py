f5f6fb89c6edbc1239a697a518745b1eb3c54d7f
Agata-Lasek <264229@student.pwr.edu.pl>
2023-12-15 00:03:47 +0100

    Update Titanic.py

--- Titanic.py
+++ Titanic.py
@@ -8,10 +8,18 @@ from keras.layers import Dense
 
 titanic = pd.read_csv('C:/Users/agata/Desktop/titanic.csv')
 test = pd.read_csv('C:/Users/agata/Desktop/test.csv')
+
+titanic['Age'] = titanic['Age'].fillna(titanic['Age'].median())         #aby uzupełnić brakujące dane w kolumnie 'Age'
+test['Age'] = test['Age'].fillna(titanic['Age'].median())
+
 #df = titanic.append(test , ignore_index = True)        #laczy 2 ramy titanic i test i ignoruje indeksy obu ramek danych i przypisuje nowe indeksy od zera
 titanic.shape, test.shape, titanic.columns.values       # informacje o liczbie wierszy, kolumn oraz nazw kolumn dla titanic, test, i połączonej ramy danych df
 titanic['WithSb'] = titanic.apply(lambda row: '1' if row['SibSp'] == 1 or row['Parch'] == 1 else 0, axis=1)       # nowa kolumna 'WithSomebody' wykonujaca OR dla SibSp i Parch (jest z kims/sam)
 test['WithSb'] = test.apply(lambda row: '1' if row['SibSp'] == 1 or row['Parch'] == 1 else 0, axis=1)      
+
+#zmiana z ciagow znakow na liczby
+titanic['WithSb'] = titanic['WithSb'].astype(int)
+test['WithSb'] = test['WithSb'].astype(int)
 
 
 print(titanic.shape, test.shape, titanic.columns.values, test.columns.values)    #sprawdza czy kolumny sa odseparowane oraz ilosc wierszy/kolumn ('.shape' inny widok np (891, 13))
@@ -20,7 +28,7 @@ print(titanic.shape, test.shape, titanic.columns.values, test.columns.values)   
 #warstwy ukryte
 
 titanic.Sex = titanic.Sex.map({'male':0, 'female':1})                            #map() przyporządkowuje wartości 0 i 1 na podstawie płci, dla uczenia maszynowego wymagane sa numeryczne dane
-test['Sex'] = test['Sex'].map({'male': 0, 'female': 1})
+test.Sex = test.Sex.map({'male': 0, 'female': 1})
 selected_columns1 = titanic[['Sex', 'Survived']] 
 grouped_by_class1 = selected_columns1.groupby(['Sex'], as_index=False).mean()    #grupuje według unikalnych wartości w wybranej kolumnie i oblicza średnią dla każdej zgrupowanej klasy (sam sprawdza co sie powtarza)
 print("\n 0 = MALE   1 = FEMALE \n",grouped_by_class1)
@@ -41,29 +49,37 @@ print("\n 1 = A   2 = B   3 = C \n",grouped_by_class3)
 
 selected_columns4 = titanic[['WithSb', 'Survived']]                               #WithSb - byl sam na pokladzie, byl z kims
 grouped_by_class4 = selected_columns4.groupby(['WithSb'], as_index=False).mean()  #!!!!!!!!musze zmienic wczytywanie gdy ktos jest z kilkoma osobami SibSp  Parch >1
-grouped_by_class4
 #print("\n 0 = ALONE   1 = TOGETHER   \n",grouped_by_class4)
 
 
 #warstwa wyjsciowa
 
-titanic = titanic.drop(labels=['Name','Cabin','Ticket','Fare','Embarked'], axis=1) #usuwamy nieuzywane kolumny, axis=1 oznacza, że usuwamy kolumny, nie wiersze
-test = test.drop(labels=['Name','Cabin','Ticket','Fare','Embarked'], axis=1) #usuwamy nieuzywane kolumny, axis=1 oznacza, że usuwamy kolumny, nie wiersze
+titanic = titanic.drop(labels=['Name','Cabin','Ticket','Fare','Embarked', 'SibSp', 'Parch'], axis=1) #usuwamy nieuzywane kolumny, axis=1 oznacza, że usuwamy kolumny, nie wiersze
+test = test.drop(labels=['Name','Cabin','Ticket','Fare','Embarked','SibSp', 'Parch'], axis=1) #usuwamy nieuzywane kolumny, axis=1 oznacza, że usuwamy kolumny, nie wiersze
 
 # Przygotowanie danych do modelu
-X_train = titanic.drop(['Survived', 'PassengerId'], axis=1)
-y_train = titanic['Survived']
+X_train = titanic.drop(['Survived'], axis=1)
+y_train = titanic['Survived'].astype(int)
+
+print(titanic.head)
+print(test.head)
+
+print(X_train.head)
+print(y_train.head)
 
 # Inicjalizacja modelu
 model = Sequential()                                                                    #Sequential służy do inicjalizacji sieci neuronowej
 
-model.add(Dense(units=13, input_dim=13, activation='relu'))                             #tworze I warstwe sieci (ile wejsc #Dense służy do dodawania warstw do sieci neuronowej.
-model.add(Dense(units=7, activation='relu'))                                            #dwie warstwy ukryte
-model.add(Dense(units=7, activation='relu'))  
+model.add(Dense(units=5, input_dim=5, activation='relu'))                             #tworze I warstwe sieci (ile wejsc #Dense służy do dodawania warstw do sieci neuronowej.
+model.add(Dense(units=3, activation='relu'))                                            #dwie warstwy ukryte
+model.add(Dense(units=3, activation='relu'))  
 model.add(Dense(units=1, activation='sigmoid'))                                         # Sigmoid dla problemu binarnej klasyfikacji, warstwa wyjsciowa
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])       # Kompilacja modelu
 
-model.fit(titanic, batch_size = 32, epochs = 50)                # Trenowanie modelu     #Trenowanie, czyli dane treningowe zostaną przetworzone 50 razy przez cały model
+#print('aaaaaaaaaaaaaaaaaaaaaaaaaaaa',X_train.shape)  #sprawdzenie poprawnosci
+#print('bbbbbbbbbbbbbbbbbbbbbbbbbbb',test.shape)
+
+model.fit(X_train, y_train, batch_size=32, epochs=50)                # Trenowanie modelu     #Trenowanie, czyli dane treningowe zostaną przetworzone 50 razy przez cały model
 
 
 #test jak sobie radzi siec  i zapisania ich do pliku CSV
